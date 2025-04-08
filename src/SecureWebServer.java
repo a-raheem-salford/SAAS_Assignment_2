e669bb5d96a78e0c0247813ec768e052e251cbf9
a-raheem-salford <a.raheem2@edu.salford.ac.uk>
2025-04-09 03:54:13 +0500

    project setup

---
+++ src/SecureWebServer.java
@@ -0,0 +1,8 @@
+public class SecureWebServer {
+
+    public static void main(String[] args) {
+        System.out.println("Secure Web Server is running...");
+        // Your server code here
+    }
+    
+}
